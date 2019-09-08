25dcd5629d4257f18a746919e96535f86d2959ef
Andreas Herz <andreas.herz@sap.com>
2019-09-08 23:50:09 +0200

    no message

--- src/main.py
+++ src/main.py
@@ -23,33 +23,15 @@ motion = MotionDetector(images_from_recorder, images_from_motion, images_to_show
 predict = ObjectDetector(images_from_motion, images_from_predict, images_to_show)
 sender = Sender(images_from_predict, None, images_to_show)
 
-index =0
 while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
     # Capture frame-by-frame
-<<<<<<< HEAD
-    cap = cv2.VideoCapture(index)
-    ret, image = cap.read()
-    cap.release()
-
-    #image = cv2.flip( image, 0 )
-
-    predictions = predict(image, predict_ids.CAT)
-    for p in predictions:
-        draw_prediction(image, p)
-
-    cv2.imshow("object detection", image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    index = (index+1)%2
-=======
     _, image = cap.read()
 
     if images_from_cam.full():
         images_from_cam.get_nowait()
-        continue
 
     images_from_cam.put((image, None))
 
@@ -61,7 +43,6 @@ while True:
         cv2.imshow("object detection", dbg)
     except:
         pass
->>>>>>> 9c764cd128a191b34c0871e0cf3fc4aa04c75e73
 
 cap.release()
 cv2.destroyAllWindows()
