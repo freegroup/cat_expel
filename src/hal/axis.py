aed7e782297ab5d41dbd9de8ad7f740fdb59b833
Andreas Herz <a.herz@freegroup.de>
2020-01-10 22:44:48 +0100

    ...

--- src/hal/axis.py
+++ src/hal/axis.py
@@ -9,13 +9,14 @@ class Axis():
 
     def center(self):
         print("center...")
-        self.set_angle(150)
+        self.set_angle(0)
 
     def set_angle(self, angle):
         angle = angle+150
+        print(angle)
         self.motor1.set_angle(angle)
         if self.motor2 is not None:
             self.motor2.set_angle(angle)
  
     def off(self):
-        self.set_angle(150)
+        self.set_angle(0)
